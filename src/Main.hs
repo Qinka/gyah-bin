760f08fd7fde6f80e68e8bdeaf40d03befaa0146
Qinka <qinka@live.com>
2015-06-16 21:12:00 +0800

    it can be released ,, version 0.2.2.0

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,19 @@ import System.Environment
 
 main :: IO()
 main = do
-        args <- getArgs
+        args<- getArgs
         gyahMain args
+        let x = if length args >=1 then args !! 0 else "help"
+        if x == "help" then do
+                putStrLn $ unlines [
+                        "\tgyah-bin version 0.2.2.0",
+                        "\tgyah-bin 版本 0.2.2.0",
+                        "",
+                        "\tHomepage https://github.com/Qinka/gyah-bin/",
+                        "\tBug report: https://github.com/Qinka/gyah-bin/issues",
+                        "\t主页  https://github.com/Qinka/gyah-bin/",
+                        "\tBUG报告  https://github.com/Qinka/gyah-bin/issues",
+                        ""
+                    ]
+            else putStr ""
         return ()
